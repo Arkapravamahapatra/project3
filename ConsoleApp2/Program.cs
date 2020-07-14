fdda4a7b3a4cc03da4e3f542e40f0a60925db0b0
arkapravamahapatra <arkapravamahapatra01@gmail.com>
2020-07-14 15:07:49 +0530

    initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, sum, avg;
+            Console.WriteLine("Enter first, second and third number");
+            a = Convert.ToInt16(Console.ReadLine());
+            b = Convert.ToInt16(Console.ReadLine());
+            c = Convert.ToInt16(Console.ReadLine());
+            sum = a + b + c;
+            avg = sum / 3;
+            Console.WriteLine(sum);
+            Console.WriteLine(avg);
+        }
+    }
+}
